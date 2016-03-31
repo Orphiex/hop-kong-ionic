3f547f5e4ffd605d4f311c87e12a763651252376
Judah Frankel <judahfrankel@gmail.com>
2016-03-31 15:39:02 +0800

    Testing...

--- www/js/controllers/login_controller.js
+++ www/js/controllers/login_controller.js
@@ -1,5 +1,25 @@
 angular.module('hopKongIonic')
 
-.controller('LoginCtrl', ['$scope', function($scope){
+.controller('LoginCtrl', ['$scope', '$location', 'UserSession', '$ionicPopup', '$rootScope', function($scope, $location, UserSession, $ionicPopup, $rootScope){
+
+  $scope.data = {};
+
+  $scope.login = function() {
+    var user_session = new UserSession({ user: $scope.data });
+    user_session.$save(
+      function(data){
+        window.localStorage['userId'] = data.id;
+        window.localStorage['userName'] = data.name;
+        $location.path('/tab/dash');
+      },
+      function(err){
+        var error = err["data"]["error"] || err.data.join('. ')
+        var confirmPopup = $ionicPopup.alert({
+          title: 'An error occured',
+          template: error
+        });
+      }
+    );
+  };
 
 }]);
--- www/js/controllers/signup_controller.js
+++ www/js/controllers/signup_controller.js
@@ -1,5 +1,16 @@
 angular.module('hopKongIonic')
 
 .controller('SignupCtrl', ['$scope', function($scope){
-
+  $scope.signup = function () {
+    $auth.submitRegistration($scope.registrationForm)
+    .then(function(resp) {
+      // handle success response
+      console.log(resp);
+      // redirect back to root when registration succesfull
+      $location.path('/');
+    }).catch(function(resp) {
+      // handle error response
+      console.log(resp);
+    });
+  };
 }]);
