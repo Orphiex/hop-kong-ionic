7e1856d09dfda08ac82052094f3c8ac908cc25bd
Julien Waite <julienwaite@gmail.com>
2016-04-01 17:42:59 +0800

    icons redirect to external websites

--- www/js/controllers/vendor_details_controller.js
+++ www/js/controllers/vendor_details_controller.js
@@ -1,9 +1,13 @@
 angular.module('hopKongIonic')
 
-.controller('VendorDetailsCtrl', ['$scope', 'VendorDetailsResource', '$localStorage', '$http', '$stateParams', function($scope, VendorDetailsResource, $localStorage, $http, $stateParams) {
+.controller('VendorDetailsCtrl', ['$scope', 'VendorDetailsResource', '$localStorage', '$http', '$stateParams', '$window', function($scope, VendorDetailsResource, $localStorage, $http, $stateParams, $window) {
   console.log($stateParams.vendor_id);
   VendorDetailsResource.get({id: $stateParams.vendor_id}).$promise.then(function(response){
     $scope.vendor = response;
     console.log(response);
   });
+
+  $scope.toBrowser = function (link) {
+    $window.open(encodeURI(link), '_system');
+  };
 }]);
