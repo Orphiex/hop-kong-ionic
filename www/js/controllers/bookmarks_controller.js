3287b83ad163e516b85a9af2a839cc7e2edce49b
Judah Frankel <judahfrankel@gmail.com>
2016-04-06 20:41:15 +0800

    testing Heroku-phone interaction

--- www/js/controllers/bookmarks_controller.js
+++ www/js/controllers/bookmarks_controller.js
@@ -2,6 +2,8 @@ angular.module('hopKongIonic')
 
 .controller('BookmarksCtrl', ['$scope', 'BeerBookmarksResource', 'VendorBookmarksResource',function($scope, BeerBookmarksResource, VendorBookmarksResource){
 
+  BeerBookmarksResource.query().$promise.then(function(resp){
 
+  });
 
 }]);
