c50bf7c2b086d81e0b29a72ee2c7bf3c757d7e58
Julien Waite <julienwaite@gmail.com>
2016-04-06 15:16:04 +0800

    removed concertina effect on accordion plus updated other pages with html & css

--- www/js/controllers/bars_results_controller.js
+++ www/js/controllers/bars_results_controller.js
@@ -1,46 +1,65 @@
 angular.module('hopKongIonic')
 
-.controller('BarsResultsCtrl', function ($scope, BarsResultsResource, $localStorage, $http, $auth) {
+.controller('BarsResultsCtrl', function ($scope, BarsResultsResource, $localStorage, $http, $auth, $cordovaGeolocation, DistanceCalc) {
   console.log($localStorage.selectedBarGroups);
 
   // $scope.list = [];
+  var options = {timeout: 5000, enableHighAccuracy: true};
 
-  $http({
-    method: 'GET',
-    // update for Heroku
-    url: "http://localhost:3000/api/bars_results.json",
-    paramSerializer: '$httpParamSerializerJQLike',
-    params: $localStorage.selectedBarGroups
-  }).then(function (resp) {
-    //console.log(resp.data);
-    $scope.vendors = resp.data;
-    //console.log($scope.list);
-  }, function (resp) {
-    //console.log(resp);
-  });
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
 
-  // $scope.populateList = function(){
-  //   for (var i = 0; i <= 9; i++) {
-  //     if ($scope.vendors.length > 0){
-  //       $scope.list.push($scope.vendors.splice(0,1));
-  //     }
-  //   }
-  //   $scope.$broadcast('scroll.infiniteScrollComplete');
-  // };
+    console.log('test');
 
-  // $scope.canWeLoadMoreContent = function() {
-  //   return ($scope.vendors.length > 0) ? true : false;
-  // };
+    var userLat  = position.coords.latitude;
+    var userLong = position.coords.longitude;
 
-  // $scope.populateList();
+    $http({
+      method: 'GET',
+      // update for Heroku
+      url: "http://localhost:3000/api/bars_results.json",
+      paramSerializer: '$httpParamSerializerJQLike',
+      params: $localStorage.selectedBarGroups
+    }).then(function (resp) {
+      console.log(resp);
+      var distanceArray = resp.data.map(addDistance);
+      $scope.vendors = distanceArray.sort(DistanceCalc.compare);
+      //console.log($scope.list);
+    }, function (resp) {
+      //console.log(resp);
+    });
 
-  // code below hides bookmark if user is authenticated
-  $auth.validateUser().then(function(resp){
-    $scope.user = resp;
-    console.log("Logged In");
-  }).catch(function(resp){
-    $scope.user = null;
-    console.log("Not Logged In");
+    function addDistance(bar){
+      if (bar.latitude === 0 && bar.longitude === 0){
+        bar.distance = "Unknown";
+      } else {
+        bar.distance = DistanceCalc.calcDistance(userLat, userLong, bar.latitude, bar.longitude);
+      }
+      return bar;
+    }
+
+    // $scope.populateList = function(){
+    //   for (var i = 0; i <= 9; i++) {
+    //     if ($scope.vendors.length > 0){
+    //       $scope.list.push($scope.vendors.splice(0,1));
+    //     }
+    //   }
+    //   $scope.$broadcast('scroll.infiniteScrollComplete');
+    // };
+
+    // $scope.canWeLoadMoreContent = function() {
+    //   return ($scope.vendors.length > 0) ? true : false;
+    // };
+
+    // $scope.populateList();
+
+    // code below hides bookmark if user is authenticated
+    $auth.validateUser().then(function(resp){
+      $scope.user = resp;
+      console.log("Logged In");
+    }).catch(function(resp){
+      $scope.user = null;
+      console.log("Not Logged In");
+    });
   });
 });
 
