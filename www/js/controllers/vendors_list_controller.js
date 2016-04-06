b3be19f04de99085218031ee1f735afb29dff1d7
Julien Waite <julienwaite@gmail.com>
2016-04-06 23:23:13 +0800

    Vendor bookmarks completed

--- www/js/controllers/vendors_list_controller.js
+++ www/js/controllers/vendors_list_controller.js
@@ -38,10 +38,37 @@ angular.module('hopKongIonic')
     }
   });
 
+  // hides bookmark if user authenticated
   $auth.validateUser().then(function(resp){
     $scope.user = resp;
+    $scope.loggedIn = resp.signedIn;
+    console.log("Logged In");
+    getResults();
   }).catch(function(resp){
-    $scope.user = null;
+    $scope.user = {id: 0};
+    $scope.loggedIn = false;
     console.log("Not Logged In");
+    getResults();
   });
+
+  function getResults(){
+    VendorDetailsResource.get({id: $stateParams.vendor_id}).$promise.then(function(response){
+      $scope.vendor = response;
+      console.log(response);
+    });
+  }
+
+  // adds a bar bookmark
+  $scope.addBookmark = function(bar_id){
+    VendorBkmkService.addBarBookmark($scope.user.id, bar_id).then(function(){
+      getResults();
+    });
+  };
+
+  // adds a bar bookmark
+  $scope.deleteBookmark = function(bkmk_id){
+    VendorBkmkService.removeBarBookmark(bkmk_id).then(function(){
+      getResults();
+    });
+  };
 }]);
